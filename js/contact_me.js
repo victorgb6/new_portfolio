b495c1485dab08a90c740aac6d373e22cf4a253f
Victor Gil <victorgb6@gmail.com>
2015-03-22 17:54:30 +0100

    Final commit

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "http://victorgil.me/portfolio2/mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
